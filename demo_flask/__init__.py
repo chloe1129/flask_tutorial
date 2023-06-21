93e9972e9205a54575f676914b82ddf7d77e43c2
Chaehy <chlee7670@cau.ac.kr>
2023-06-21 16:56:24 -0700

    add: args to html

--- demo_flask/__init__.py
+++ demo_flask/__init__.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    args = {'title':'index page', 'contents': 'hello world'}
+# variable: <variable-name>
+@app.route("/hello/<name>")
+def hello(name):
+    args = {'title':'This is index page', 'contents': 'hello %s !' % name}
     return render_template('index.html', **args)
+    # return 'Hello %s :)' % name
+
+if __name__ =='__main__':
+    app.run()
